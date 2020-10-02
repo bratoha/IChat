6375c52d189c17d0730c1ebd2a9dd284f9b2a5b5
aikalinin <bratohalo@gmail.com>
2020-10-02 14:43:14 +0300

    Added constraints for AuthViewController

--- IChat/AuthViewController.swift
+++ IChat/AuthViewController.swift
@@ -22,7 +22,32 @@ class AuthViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let googleView = ButtonFormView(label: googleLabel, button: googleButton)
+        let emailView = ButtonFormView(label: emailLabel, button: emailButton)
+        let loginView = ButtonFormView(label: alreadyOnboardLabel, button: loginButton)
+        
+        let stackView = UIStackView(arrangedSubviews: [googleView, emailView, loginView], axis: .vertical, spacing: 40)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(logoImageView)
+        view.addSubview(stackView)
+        
+        
+        logoImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 160).isActive = true
+        logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 160),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
     }
 
 
@@ -33,7 +58,10 @@ import SwiftUI
 struct AuthViewControllerProvider: PreviewProvider {
     
     static var previews: some View {
-        ContainerView().edgesIgnoringSafeArea(.all)
+        Group {
+            ContainerView().edgesIgnoringSafeArea(.all)
+            ContainerView().edgesIgnoringSafeArea(.all)
+        }
     }
     
     struct ContainerView: UIViewControllerRepresentable {
---
+++ IChat/Extensions/UIKit/UIStackView+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIStackView+Extension.swift
+//  IChat
+//
+//  Created by Антон Калинин on 02.10.2020.
+//
+
+import UIKit
+
+extension UIStackView {
+    
+    convenience init(arrangedSubviews: [UIView], axis: NSLayoutConstraint.Axis, spacing: CGFloat) {
+        self.init(arrangedSubviews: arrangedSubviews)
+        self.axis = axis
+        self.spacing = spacing
+    }
+    
+    
+}
