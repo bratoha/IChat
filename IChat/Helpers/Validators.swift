31203f4719a9e5c6392f7acd98f90748c7b247f9
aikalinin <bratohalo@gmail.com>
2020-11-03 12:13:29 +0300

    Added validator to login and registration fields

---
+++ IChat/Helpers/Validators.swift
@@ -0,0 +1,35 @@
+//
+//  Validators.swift
+//  IChat
+//
+//  Created by Антон Калинин on 03.11.2020.
+//
+
+import Foundation
+
+class Validators {
+    
+    static func isFilled(email: String?, password: String?, confirmPassword: String?) -> Bool {
+        guard let password = password,
+              let confirmPassword = confirmPassword,
+              let email = email,
+              !email.isEmpty,
+              !confirmPassword.isEmpty,
+              !password.isEmpty else {
+            return false
+        }
+        
+        return true
+    }
+    
+    static func isSimpleEmail(_ email: String) -> Bool {
+        let emailRegEx = "^.+@.+\\..{2,}$"
+        return check(text: email, regEx: emailRegEx)
+    }
+    
+    private static func check(text: String, regEx: String) -> Bool {
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regEx)
+        return predicate.evaluate(with: text)
+    }
+    
+}
--- IChat/Services/AuthService.swift
+++ IChat/Services/AuthService.swift
@@ -13,7 +13,16 @@ class AuthService {
     private let auth = Auth.auth()
 
     func login(email: String?, password: String?, completion: @escaping (Result<User, Error>) -> Void) {
-        auth.signIn(withEmail: email!, password: password!) { (result, error) in
+        
+        guard let email = email,
+              let password = password,
+              !email.isEmpty,
+              !password.isEmpty else {
+            completion(.failure(AuthError.notFilled))
+            return
+        }
+        
+        auth.signIn(withEmail: email, password: password) { (result, error) in
             guard let result = result else {
                 completion(.failure(error!))
                 return
@@ -24,6 +33,22 @@ class AuthService {
     }
     
     func register(email: String?, password: String?, confirmPassword: String?, completion: @escaping (Result<User, Error>) -> Void) {
+        
+        guard Validators.isFilled(email: email, password: password, confirmPassword: confirmPassword) else {
+            completion(.failure(AuthError.notFilled))
+            return
+        }
+        
+        guard password! == confirmPassword! else {
+            completion(.failure(AuthError.passwordNotMatched))
+            return
+        }
+        
+        guard Validators.isSimpleEmail(email!) else {
+            completion(.failure(AuthError.invalidEmail))
+            return
+        }
+        
         auth.createUser(withEmail: email!, password: password!) { (result, error) in
             guard let result = result else {
                 completion(.failure(error!))
